b9d609c39d887fcbaaf26fd6f88951b157a4fa56
Ngenko <jesusdbarajas@gmail.com>
2018-05-04 15:28:38 -0500

    Realizando las secciones

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var btnMenu = document.getElementById("boton-menu");
+var navegacion = document.getElementById("navegacion");
+
+btnMenu.addEventListener('click', function(){
+    navegacion.classList.toggle('mostrar');
+})
